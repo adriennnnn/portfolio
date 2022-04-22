fdc4cb549d634d0815ba1f3ea6b42f3036c0af92
adriennnnn <95212982+adriennnnn@users.noreply.github.com>
2022-04-22 11:28:30 +0200

    final sans responsive

--- main.js
+++ main.js
@@ -10,10 +10,28 @@ $(window).scroll(function() {
 
 btn.on('click', function(e) {
   e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
+  $('html, body').animate({scrollTop:0}, '500');
 });
 
 function copyFunction() {
   navigator.clipboard.writeText('adrienthomas54@gmail.com')
   // alert("texte copier")
+  divCacher.style.display = "block";
+  setTimeout(() => { console.log("cacher!");
+  divCacher.style.display = "none";
+
+}, 2000);
 }
+
+
+// let btnE = document.getElementById("btnE");
+// let divCacher = document.getElementById("divCacher");
+
+// function afficher(){
+//   console.log('ttt');
+//   if(getComputedStyle(divCacher).display != "none"){
+//     divCacher.style.display = "none";
+//   } else {
+//     divCacher.style.display = "block";
+//   }
+// };
